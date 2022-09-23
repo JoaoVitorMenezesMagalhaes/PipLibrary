97cf16718c0db1745c62b76761ef0ce8cfc6a852
JoaoVitorMenezesMagalhaes <joaovmm@al.insper.edu.br>
2022-09-22 21:16:20 -0300

    tradução

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from datetime import date, datetime
+from babel.dates import format_datetime
+import gettext
+gettext.install('cli', localedir='locale') 
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    print(_('Último commit feito em:'), date, _(' por'), name)
